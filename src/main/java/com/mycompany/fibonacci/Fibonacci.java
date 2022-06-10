0670f5f5958cb8454822bd09a6cdbd3448d69bb8
Rafael Andres Daza <radazn1998@gmail.com>
2022-06-09 19:25:19 -0500

    Refactorización de algoritmo Fibonacci y ejecución de casos de prueba

--- src/main/java/com/mycompany/fibonacci/Fibonacci.java
+++ src/main/java/com/mycompany/fibonacci/Fibonacci.java
@@ -15,12 +15,18 @@ public class Fibonacci {
     public static ArrayList<Integer> CalcularSerie(int n) {
         int num1 = 0, num2 = 1, suma = 1;
         ArrayList<Integer> serie = new ArrayList<>();
-        serie.add(num1);  
-        for (int i = 1; i < n; i++) {
-            serie.add(suma);
-            suma = num1 + num2;
-            num1 = num2;
-            num2 = suma;
+        if(n == 0){
+            return null;
+        }else if(n == 1){
+            serie.add(num1);
+        }else if(n >= 2){
+            serie.add(num1);
+            for (int i = 1; i < n; i++) {
+                serie.add(suma);
+                suma = num1 + num2;
+                num1 = num2;
+                num2 = suma;
+            }    
         }
         return serie;
     }
