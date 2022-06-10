fd6d57128e85453b07027884dcf533d1308d5ed6
Rafael Andres Daza <radazn1998@gmail.com>
2022-06-09 19:08:42 -0500

    Implementación de algoritmo Fibonacci y ejecución de casos de prueba

--- src/main/java/com/mycompany/fibonacci/Fibonacci.java
+++ src/main/java/com/mycompany/fibonacci/Fibonacci.java
@@ -9,10 +9,19 @@ import java.util.ArrayList;
 public class Fibonacci {
 
     public static void main(String[] args) {
-        
+        System.out.println(CalcularSerie(6));
     }
 
-    public static ArrayList<Integer> CalcularSerie() {
-        return null;
+    public static ArrayList<Integer> CalcularSerie(int n) {
+        int num1 = 0, num2 = 1, suma = 1;
+        ArrayList<Integer> serie = new ArrayList<>();
+        serie.add(num1);  
+        for (int i = 1; i < n; i++) {
+            serie.add(suma);
+            suma = num1 + num2;
+            num1 = num2;
+            num2 = suma;
+        }
+        return serie;
     }
 }
--- src/test/java/com/mycompany/fibonacci/FibonacciTest.java
+++ src/test/java/com/mycompany/fibonacci/FibonacciTest.java
@@ -1,5 +1,6 @@
 package com.mycompany.fibonacci;
 
+import java.util.ArrayList;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,6 +12,25 @@ public class FibonacciTest {
     
     @Test
     public void testFibonacci() {
-        assertEquals(true,Fibonacci.CalcularSerie());            
-    }   
+        ArrayList<Integer> serie = new ArrayList<>();
+        serie.add(0);
+        serie.add(1);
+        serie.add(1);
+        serie.add(2);
+        serie.add(3);
+        serie.add(5);
+        assertEquals(serie,Fibonacci.CalcularSerie(6));            
+    } 
+    
+    @Test
+    public void testFibonacci2() {
+        ArrayList<Integer> serie = new ArrayList<>();
+        serie.add(0);
+        serie.add(1);
+        serie.add(5);
+        serie.add(2);
+        serie.add(3);
+        serie.add(5);
+        assertEquals(serie,Fibonacci.CalcularSerie(6));            
+    } 
 }
